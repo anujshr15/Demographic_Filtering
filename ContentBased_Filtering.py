19d5c1657f8bba7c1f76f0e7e70c050d73c5c3c0
Anuj Shrivastav <anujshrivastav97@gmail.com>
2019-06-20 17:51:43 +0530

    Content Based Filtering using content like cast,crew,director and genre

--- ContentBased_Filtering.py
+++ ContentBased_Filtering.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from ast import literal_eval
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 pd.set_option('display.max_columns',None)
 
@@ -32,4 +35,59 @@ def get_recommendations(title,cosine_sim=cosine_sim):
     movie_indices=[i[0] for i in sim_scores]
     return df['title_x'].iloc[movie_indices]
 
-print(get_recommendations('The Dark Knight'))
+features=['cast','crew','keywords','genres']
+for feature in features:
+    df[feature]=df[feature].apply(literal_eval)
+
+def get_director(x):
+    for i in x:
+        if(i['job']=='Director'):
+            return i['name']
+    return np.nan
+
+def get_list(x):
+    if isinstance(x,list):
+        names=[i['name'] for i in x]
+        if len(names)>3:
+            names=names[:3]
+        return names
+    return []
+
+df['director']=df['crew'].apply(get_director)
+
+features=['cast','keywords','genres']
+for feature in features:
+    df[feature]=df[feature].apply(get_list)
+
+#print(df[['title_x','cast','director','keywords','genres']].head(3))
+
+def clean_data(x):
+    if isinstance(x,list):
+        return [str.lower(i.replace(" ","")) for i in x]
+    else:
+        if(isinstance(x,str)):
+            return str.lower(x.replace(" ",""))
+        return ""
+
+features=['cast','keywords','director','genres']
+for feature in features:
+    df[feature]=df[feature].apply(clean_data)
+
+
+def create_soup(x):
+    return " ".join(x['keywords'])+" "+" ".join(x['cast'])+" "+" ".join(x['director'])+" "+" ".join(x['genres'])
+
+df['soup']=df.apply(create_soup,axis=1)
+
+count=CountVectorizer(stop_words='english')
+count_matrix=count.fit_transform(df['soup'])
+
+cosine_sim2=cosine_similarity(count_matrix,count_matrix)
+
+df=df.reset_index()
+
+indices=pd.Series(df.index,index=df['title_x'])
+
+print(get_recommendations('The Godfather',cosine_sim2))
+
+
